772c9d1e3287287328fdd4915debd3a2d43934c8
Paul ALNET <alnet@insa-toulouse.fr>
2025-05-05 13:50:51 +0200

    test(python): squared matrix result

--- python/test.py
+++ python/test.py
@@ -8,6 +8,17 @@ class TestComplexOperation(unittest.TestCase):
         with self.assertRaises(TypeError):
             result = complexmodule.complex_operation(array)
 
+    def test_result(self):
+        DIM = 1000
+        matrix = np.random.rand(DIM, DIM) + 1j * np.random.rand(DIM, DIM)
+        A = matrix.astype(np.complex128)
+
+        result = complexmodule.complex_operation(A)
+        expected = A.dot(A)
+        # expected = A.dot(np.diag(np.diag(A))).dot(np.linalg.inv(A))
+
+        np.testing.assert_allclose(result, expected)
+
 # Create a complex NumPy array
 array = np.array([[1+2j, 3+4j], [5+6j, 7+8j]], dtype=np.complex128)
 
